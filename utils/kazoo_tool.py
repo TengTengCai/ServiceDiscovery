1d3ed84302126faf29f3048b0df341780270a259
Tianjun <1021766585@qq.com>
2019-05-24 17:23:45 +0800

    修改获取子节点算法

--- utils/kazoo_tool.py
+++ utils/kazoo_tool.py
@@ -14,8 +14,6 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(default_handler)
 
 ROOT_PATH = '/platform'
-SERVICE_DISCOVERY_CONFIG_PATH = '/serviceDiscovery/config'
-MAX_ID_PATH = SERVICE_DISCOVERY_CONFIG_PATH+'/maxId'
 
 
 class KazooConn(object):
@@ -41,13 +39,10 @@ class KazooConn(object):
         self.zk = KazooClient(hosts=hosts)
         self.zk.add_listener(kazoo_conn_listener)
         self.zk.start(timeout=30)
-        logger.info(self.zk.exists(SERVICE_DISCOVERY_CONFIG_PATH))
         if not self.zk.connected:
             # Not connected, stop trying to connect
             self.close_conn()
             raise Exception("Unable to connect.")
-        if self.zk.exists(SERVICE_DISCOVERY_CONFIG_PATH):
-            self.zk.create(SERVICE_DISCOVERY_CONFIG_PATH)
 
     @property
     def zkc(self):
@@ -58,20 +53,21 @@ class KazooConn(object):
     def close_conn(self):
         self.zk.stop()
 
-    def get_all_nodes(self):
-        if not self.zk.exists(ROOT_PATH):
-            self.zk.create(ROOT_PATH)
-        _, stat = self.zk.get(ROOT_PATH)
+    def get_all_nodes(self, path):
+        if not self.zk.exists(path):
+            raise Exception("The node you are looking for does not exist!")
+        _, stat = self.zk.get(path)
+        path_name = str(path).split('/')[-1]
         node = {
             'pId': 0,
             'id': stat.czxid,
-            'name': ROOT_PATH.replace('/', ''),
+            'name': path_name,
             'open': True,
         }
         p_id = stat.czxid
         level = 0
         node_list = [node]
-        node_list = self.get_child_node(ROOT_PATH, p_id, level, node_list)
+        node_list = self.get_child_node(path, p_id, level, node_list)
         return node_list
 
     def get_child_node(self, path, p_id, level, node_list):
@@ -79,10 +75,9 @@ class KazooConn(object):
         if level > 3:
             return node_list
         children = self.zk.get_children(path)
-        logger.info(children)
         for child in children:
-            path = '{}/{}'.format(path, child)
-            _, stat = self.zk.get(path)
+            child_path = '{}/{}'.format(path, child)
+            _, stat = self.zk.get(child_path)
             node = {
                 'pId': p_id,
                 'id': stat.czxid,
@@ -92,7 +87,10 @@ class KazooConn(object):
             node_list.append(node)
             if stat.numChildren == 0:
                 continue
-            node_list = self.get_child_node(ROOT_PATH, p_id, level, node_list)
+            node_list = self.get_child_node(child_path,
+                                            stat.czxid,
+                                            level,
+                                            node_list)
         return node_list
 
 
